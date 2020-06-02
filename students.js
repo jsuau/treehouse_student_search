f23540a98315d2440c8dd5cac85da910840efa84
Joanna Suau <39948295+jsuau@users.noreply.github.com>
2020-06-02 09:55:08 +0100

    Create student.js with student data structure

---
+++ students.js
@@ -0,0 +1,33 @@
+var students = [ 
+  { 
+   name: 'Pablo',
+    track: 'R',
+    achievements: 158,
+    points: 14730
+  },
+  {
+    name: 'Joanna',
+    track: 'Front End Development',
+    achievements: 175,
+    points: 16374
+  },
+  {
+    name: 'Daniel',
+    track: 'PHP Development',
+    achievements: 55,
+    points: 2025
+  },
+  {
+    name: 'Laura',
+    track: 'Learn WordPress',
+    achievements: 40,
+    points: 1950
+  },
+  {
+    name: 'Negura',
+    track: 'Rails Development',
+    achievements: 5,
+    points: 350
+  }
+];
+
